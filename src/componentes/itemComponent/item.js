715f248d635aa335731707978e11683f3eb7e271
Michel Rocha <michelrocha502@gmail.com>
2023-10-22 22:08:55 -0300

    retirado funções nao usadas para permitir o deploy no netlify

--- src/componentes/itemComponent/item.js
+++ src/componentes/itemComponent/item.js
@@ -29,9 +29,7 @@ function Item(props) {
     }, [quantidade, valorEditado])
 
 
-    const atualizarItem = () => {
-        props.atualizarItem(props.id, nomeEditado, valorEditado, valorSomado);
-    };
+
 
     useEffect(() => {
 
